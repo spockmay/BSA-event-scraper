5ba373b29e95188aecc6cd8c5040684cb243495d
Ryan May <spockmay@users.noreply.github.com>
2026-04-19 21:48:59 -0400

    combine multiple councils and sort by date
    Closes #4 in a much simpler way

--- bsa_events.py
+++ bsa_events.py
@@ -3,18 +3,16 @@ from datetime import datetime, timedelta
 from buckeye import scrape_buckeye_calendar
 from scoutingevent_json import scrape_scoutcal_json
 
+t_now = datetime.now()
+t_end = t_now + timedelta(weeks=52)
+
+all_events = []
 
 # Scrape Buckeye
-print("Scraping events from the Buckeye Council calendar...")
+print("Scraping events from the Buckeye Council's calendar...")
 events = scrape_buckeye_calendar()
-
-if events:
-    print(f"Found {len(events)} events")
-    for event in events:
-        print("-" * 20)
-        print(event)
-else:
-    print("Failed to retrieve events.")
+print("  %s events found." % len(events))
+all_events.extend(events)
 
 # Scrape scoutcal councils
 councils = [
@@ -22,14 +20,14 @@ councils = [
     ("Great Trail Council", "19108"),
 ]
 
-t_now = datetime.now()
-t_end = t_now + timedelta(weeks=52)
-
-print("Scraping events from the Scouting Event calendar...")
-
 for council in councils:
+    print(f"Scraping events from {council[0]}'s calendar...")
     events = scrape_scoutcal_json(council[1], t_now, t_end)
-    print(council[0])
-    for event in events:
-        print("-" * 20)
-        print(event)
+    print("  %s events found." % len(events))
+    all_events.extend(events)
+
+# sort events
+all_events.sort(key=lambda x: x.start)
+for event in all_events:
+    print("-" * 20)
+    print(event)
